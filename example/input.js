7eba71ce8cce99b036b12613f1f92cc890b57ee8
nd-02110114 <nd.12021218@gmail.com>
2019-06-30 14:58:15 +0900

    :sparkles: (example) add exmaple

--- example/input.js
+++ example/input.js
@@ -1,2 +1,36 @@
 // https://v8.dev/blog/cost-of-javascript-2019
 const data = { foo: 42, bar: 1337 }
+
+/* string */
+const data2 = { foo: 'foo' };
+
+/* numeric */
+const data3 = { foo: 10 };
+
+/* boolean */
+const data4 = { foo: true };
+
+/* null */
+const data5 = { foo: null };
+
+/* array */
+const data6 = { foo: [null, 10, 'foo'] };
+
+/* nested array */
+const data7 = { foo: [null, [10, 2], [{ foo: 'foo' }]] };
+
+/* object */
+const data8 = { foo: { bar: 1337 } };
+
+
+/* invalidValue */
+const data9 = { foo: () => console.log("invalid") };
+
+const data10 = { 
+  method(arg) {
+    return arg;
+  },
+  foo: 'foo',
+};
+
+const data11 = { ...data, foo: 'foo' };
--- example/output.js
+++ example/output.js
@@ -1,2 +1,38 @@
 // https://v8.dev/blog/cost-of-javascript-2019
 const data = JSON.parse('{"foo":42,"bar":1337}');
+/* string */
+
+const data2 = JSON.parse('{"foo":"foo"}');
+/* numeric */
+
+const data3 = JSON.parse('{"foo":10}');
+/* boolean */
+
+const data4 = JSON.parse('{"foo":true}');
+/* null */
+
+const data5 = JSON.parse('{"foo":null}');
+/* array */
+
+const data6 = JSON.parse('{"foo":[null,10,"foo"]}');
+/* nested array */
+
+const data7 = JSON.parse('{"foo":[null,[10,2],[{"foo":"foo"}]]}');
+/* object */
+
+const data8 = JSON.parse('{"foo":{"bar":1337}}');
+/* invalidValue */
+
+const data9 = {
+  foo: () => console.log("invalid")
+};
+const data10 = {
+  method(arg) {
+    return arg;
+  },
+
+  foo: 'foo'
+};
+const data11 = { ...data,
+  foo: 'foo'
+};
